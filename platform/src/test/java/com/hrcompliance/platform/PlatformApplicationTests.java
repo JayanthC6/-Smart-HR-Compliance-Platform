37402b863f92d25de1600d2010caaa8796f49c18
JayanthC6 <jayanthjayanthc96@gmail.com>
2026-06-29 20:30:29 +0530

    first commit

---
+++ platform/src/test/java/com/hrcompliance/platform/PlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrcompliance.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
